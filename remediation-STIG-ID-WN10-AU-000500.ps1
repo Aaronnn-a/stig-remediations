cc00e97b48c66508cc30c48b18b7c8dda53fcb1f
Aaron <aaronashraf.aa@gmail.com>
2025-12-12 15:29:36 +0100

    Create PowerShell script for event log size compliance
    This script ensures the Windows Application event log size is at least 32 MB.

---
+++ remediation-STIG-ID-WN10-AU-000500.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Aaron Ashraf
+    LinkedIn        : https://www.linkedin.com/in/aaron-ashraf-0a24b1250/
+    GitHub          : github.com/Aaronnn-a
+    Date Created    : 2025-12-12
+    Last Modified   : 2025-12-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
